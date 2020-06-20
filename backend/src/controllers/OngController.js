84ad8af8b605227123fd668134ac87d6481b575b
Bernardo Aquino <bernardoaquinoc@gmail.com>
2020-06-20 20:41:29 -0300

    BackEnd

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,27 @@
+const connection = require('../database/connection');
+const crypto = require('crypto'); //Gera string com textos aleatórios
+
+module.exports = {
+    async index(request, response) {
+        const ongs = await connection('ongs').select('*');
+
+        return response.json(ongs);
+    },
+
+    async create(request, response) {
+        const { name, email, whatsapp, city, uf } = request.body;
+
+        const id = crypto.randomBytes(4).toString('HEX'); //Gera 4 bytes de caracteres hexadecimal
+
+        await connection('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf,
+        });
+
+        return response.json({ id });
+    }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const cors = require('cors'); //Quem pode acessar nossa aplicação
 const routes = require('./routes');
 
 const app = express();
 
+// Aplicação pronta
+// app.use(cors({
+//     origin: 'http://meuapp.com'
+// }));
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
